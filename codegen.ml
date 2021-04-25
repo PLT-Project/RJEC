ae8836c6cd4ce79d0fb0847fd4f8c910f46045d9
Justin Chen <justinbrianchen@yahoo.com>
2021-04-25 15:23:49 -0700

    re-sign codegen

--- codegen.ml
+++ codegen.ml
@@ -1,16 +1,7 @@
 (* Code generation: translate takes a semantically checked AST and
-produces LLVM IR
-
-LLVM tutorial: Make sure to read the OCaml version of the tutorial
-
-http://llvm.org/docs/tutorial/index.html
-
-Detailed documentation on the OCaml LLVM library:
-
-http://llvm.moe/
-http://llvm.moe/ocaml/
-
-*)
+ * Initially based on MicroC, with inspiration from Shoo
+ * Written by Elaine Wang, Justin Chen, Riya Chakraborty, and Caroline Hoang
+ *)
 
 module L = Llvm
 module A = Ast
